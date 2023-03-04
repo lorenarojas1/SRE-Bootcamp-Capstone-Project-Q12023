bacec9a0d23421258ca35db35d18f003a05f76e5
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-03-03 18:38:57 -0600

    feat: set up variables environment to db connection and jwt token

---
+++ SRE-Bootcamp-Capstone-Project-Q12023/node/config/index.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import process from 'process';
+
+dotenv.config();
+
+export const config = {
+  DB_HOST: process.env.DB_HOST,
+	DB_USER: process.env.DB_USER,
+	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_DATABASE: process.env.DB_DATABASE,
+	JWT_KEY: process.env.JWT_KEY,
+	APP_PORT: process.env.APP_PORT,
+}
