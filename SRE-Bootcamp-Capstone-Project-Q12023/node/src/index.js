6fe29d067c1430738c8379a309d3d8030d5e39b0
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-03-19 16:16:29 -0600

    refactor code to deploy

--- SRE-Bootcamp-Capstone-Project-Q12023/node/src/index.js
+++
@@ -1,6 +0,0 @@
-import { config } from './config';
-import  app  from './server';
-
-app.listen(config.APP_PORT, function() {
-  console.log('listening at',config.APP_PORT);
-});
--- SRE-Bootcamp-Capstone-Project-Q12023/node/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-export const index = (_, res, next) => {
-  res.status(200).render('index')
-  next();
-  let response ={
-    data: 'OK'
-  }
-  console.log(response)
-};
