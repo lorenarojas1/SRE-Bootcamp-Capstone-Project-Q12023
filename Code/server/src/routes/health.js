b88c58c0cae1993186022d17f6a1e1c7344f46e7
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-03-19 17:37:05 -0600

    fix: show status to deploy in health page

--- Code/server/src/routes/health.js
+++ Code/server/src/routes/health.js
@@ -1,21 +1,3 @@
-  import { protectFunction } from '../services/protected';
-
-export const health = async (req, res, next) => {
-    try{
-        let {jwt} = req.cookies;
-        let response = {
-          data: await protectFunction(jwt)
-        };
-        if(response.data === null){
-            res.status(401).render('unauthenticated');
-            next();
-            console.log('Unauthenticated')
-        }else{
-            res.status(200).render('health');
-            next();
-            console.log('Authenticated')
-        }
-    }catch(err){
-        return new Error(err)
-    }
+export const health = async (_, res) => {
+            res.status(200).send('OK');
   }
